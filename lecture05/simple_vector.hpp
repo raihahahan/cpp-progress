dc6fe5e5734fb01affab6f474af3b6e28725eff9
raihahahan <mraihandev@gmail.com>
2025-06-18 18:15:21 +0800

    added 0/3 constructors

--- lecture05/simple_vector.hpp
+++ lecture05/simple_vector.hpp
@@ -8,14 +8,27 @@
 template <typename ElemTy>
 class SimpleVector {
 private:
-    ElemTy* m_buffer = nullptr;
-    size_t m_size = 0;
+    ElemTy* m_buffer;
+    size_t m_size;
 
 public:
-    SimpleVector() = default;
+    #pragma region Constructors
+    SimpleVector() : m_buffer{nullptr}, m_size{0} {}
 
-    SimpleVector(std::initializer_list<ElemTy> init) {
-        m_size = init.size();
+    SimpleVector(const SimpleVector& other) : m_size{other.m_size} {
+        m_buffer = new ElemTy[m_size];
+        for (size_t i = 0; i < m_size; ++i) {
+            m_buffer = other.m_buffer[i];
+        }
+    }
+
+    SimpleVector& operator=(const SimpleVector& other) {
+        SimpleVector copy{other};
+        this->swap(copy);
+        return *this;
+    }
+
+    SimpleVector(std::initializer_list<ElemTy> init) : m_size{init.size()} {
         m_buffer = new ElemTy[m_size];
         std::copy(init.begin(), init.end(), m_buffer);
     }
@@ -23,12 +36,13 @@ public:
     ~SimpleVector() {
         delete[] m_buffer;
     }
-
+    
+    #pragma region Accessors
     size_t size() const { return m_size; }
-
     ElemTy& operator[](size_t i) { return m_buffer[i]; }
     const ElemTy& operator[](size_t i) const { return m_buffer[i]; }
 
+    #pragma region Normal Iterator
     ElemTy* begin() { return m_buffer; }
     ElemTy* end() { return m_buffer + m_size; }
     const ElemTy* begin() const { return m_buffer; }
@@ -36,7 +50,7 @@ public:
     const ElemTy* cbegin() const { return m_buffer; }
     const ElemTy* cend() const { return m_buffer + m_size; }
 
-    // --- Reverse Iterator Support ---
+    #pragma region Reverse Iterator
     lecture05::iterators::RevIter<ElemTy*> rbegin() { return {end()}; }
     lecture05::iterators::RevIter<ElemTy*> rend() { return {begin()}; }
     lecture05::iterators::RevIter<const ElemTy*> rbegin() const { return {end()}; }
@@ -44,7 +58,16 @@ public:
     lecture05::iterators::RevIter<const ElemTy*> crbegin() const { return {end()}; }
     lecture05::iterators::RevIter<const ElemTy*> crend() const { return {begin()}; }
 
-    // --- insert_back using concepts and traits ---
+    #pragma region Methods
+    void swap(SimpleVector& other) {
+        std::swap(m_buffer, other.m_buffer);
+        std::swap(m_size, other.m_size);
+    }
+
+    friend void swap(SimpleVector& a, SimpleVector& b) {
+        a.swap(b);
+    }
+
     void push_back(const ElemTy& val) {
         ElemTy* new_buffer = new ElemTy[m_size + 1];
         std::copy(m_buffer, m_buffer + m_size, new_buffer);
@@ -54,14 +77,23 @@ public:
         m_size++;
     }
 
+    void push_front(const ElemTy& val) {
+        // TODO
+        val;
+    }
+
+    ElemTy pop_back() {
+        // TODO
+        return nullptr;
+    }
+
+    ElemTy pop_front() {
+        // TODO
+        return nullptr;
+    }
+
     lecture05::iterators::BackIns<SimpleVector> back_inserter() {
         return {this};
-    }
-    
-    template <typename InputIt>
-    // requires std::input_iterator<InputIt>
-    void insert_back(InputIt begin, InputIt end) {
-        std::copy(begin, end, back_inserter());
     }
 
     template <typename ForwardIt>
@@ -78,6 +110,7 @@ public:
         m_size += num_elems;
     }
 
+    #pragma region Operators
     friend std::ostream& operator<<(std::ostream& os, const SimpleVector& v) {
         os << "[";
         for (size_t i = 0; i < v.size(); ++i) {
